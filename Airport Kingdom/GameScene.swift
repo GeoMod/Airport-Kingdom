bd005a58a7823145a05ab9dece03cdc6fc50e17b
Dan O'Leary <impulsecoupled@gmail.com>
2019-06-07 11:47:58 -0500

    Aircraft rotation controls are taking shape. Airplane rotates about the Y axis "properly", but in the opposite direction.

--- Airport Kingdom/GameScene.swift
+++ Airport Kingdom/GameScene.swift
@@ -14,12 +14,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     weak var viewController: GameViewController!
     
+    let background = SKSpriteNode(imageNamed: "background")
+    let runway = SKSpriteNode(imageNamed: "runway")
     let yokeBase = SKSpriteNode(imageNamed: "yokeBase")
     let yoke = SKSpriteNode(imageNamed: "yoke")
     var airplane: SKSpriteNode!
+    var direction = float2(0,0)
     
     var didTouchYoke = false
     var touchDegrees = CGFloat(0)
+    
     
     // hacking together some airplane positions.
     var posX: CGFloat = 256.0
@@ -31,13 +35,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     
     func setUpGameScene() {
-        let background = SKSpriteNode(imageNamed: "background")
         background.position = CGPoint(x: 512, y: 384)
         background.blendMode = .replace
         background.zPosition = -1
         addChild(background)
         
-        let runway = SKSpriteNode(imageNamed: "runway")
         runway.anchorPoint = CGPoint(x: 0, y: 0)
         runway.position = CGPoint(x: 236, y: 180)
         runway.blendMode = .replace
@@ -58,19 +60,22 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         addChild(yoke)
         
         airplane = SKSpriteNode(imageNamed: "airplane")
-        
         airplane.physicsBody = SKPhysicsBody(texture: airplane.texture!, size: airplane.size)
         airplane.physicsBody?.isDynamic = true
         
-        airplane.zRotation = rad2deg(-90.5)  // -90 was orginal value
+        airplane.zRotation = rad2deg(-90.5)
         airplane.position = CGPoint(x: (runway.position.x + airplane.size.width) - 10, y: (runway.position.y + airplane.size.height) + 10)
         airplane.zPosition = 1
         addChild(airplane)
     }
 
-    
-    func rad2deg(_ number: Double) -> CGFloat {
-        return CGFloat(number * 180 / .pi)
+    func virtualDPad() -> CGRect {
+        var vDpad = CGRect(x: 0, y: 0, width: yokeBase.frame.width, height: yokeBase.frame.height)
+
+        vDpad.origin.x = yokeBase.position.x - yokeBase.frame.size.width / 2
+        vDpad.origin.y = yokeBase.position.y - yokeBase.frame.size.height / 2
+
+        return vDpad
     }
     
     func touchUp(atPoint pos : CGPoint) {
@@ -110,16 +115,26 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                     yoke.position = CGPoint(x: yokeBase.position.x - xDistance, y: yokeBase.position.y + yDistance)
                 }
                 
-                if xDistance < 0 {
-                    
-                    let clockwiseRotation = SKAction.rotate(byAngle: -.pi / 4, duration: 1.5)
-                    airplane.run(clockwiseRotation, withKey: "rotation")
-                } else if xDistance > 0 {
-                    
-                    let counterClockwiseRotation = SKAction.rotate(byAngle: .pi / 4, duration: 4.5)
-                    airplane.run(counterClockwiseRotation, withKey: "rotation")
+                if virtualDPad().contains(location) {
+                    let middleOfCircleX = virtualDPad().origin.x + 75
+                    let middleOfCircleY = virtualDPad().origin.y + 75
+                    let lengthOfX = Float(location.x - middleOfCircleX)
+                    let lengthOfY = Float(location.y - middleOfCircleY)
+                    direction = float2(x: lengthOfX, y: lengthOfY)
+                    direction = normalize(direction)
+                    let degree = atan2(direction.x, direction.y)
+                    directionAngle = CGFloat(degree)
                 }
-//                airplane.zRotation = rad2deg(Double(xDistance))
+                
+//                if xDistance < 0 {
+//                    let clockwiseRotation = SKAction.rotate(byAngle: -.pi / 4, duration: 1.5)
+//                    airplane.run(clockwiseRotation, withKey: "rotation")
+//                } else if xDistance > 0 {
+//
+//                    let counterClockwiseRotation = SKAction.rotate(byAngle: .pi / 4, duration: 4.5)
+//                    airplane.run(counterClockwiseRotation, withKey: "rotation")
+//                }
+                airplane.zRotation = directionAngle // rad2deg(Double(xDistance))
                 
             }
         }
@@ -133,6 +148,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             didTouchYoke = false
         }
     }
+    
+    
+    var directionAngle: CGFloat = 0.0 {
+        didSet {
+            if directionAngle != oldValue {
+                // action that rotates the node to an angle in radian.
+                let action = SKAction.rotate(toAngle: directionAngle, duration: 0.1)
+                run(action)
+            }
+        }
+    }
+    
+    
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
---
+++ Airport Kingdom/HelperMethods.swift
@@ -0,0 +1,14 @@
+//
+//  HelperMethods.swift
+//  Airport Kingdom
+//
+//  Created by Daniel O'Leary on 6/6/19.
+//  Copyright © 2019 Impulse Coupled Dev. All rights reserved.
+//
+
+import SpriteKit
+
+func rad2deg(_ number: Double) -> CGFloat {
+    return CGFloat(number * 180 / .pi)
+}
+
